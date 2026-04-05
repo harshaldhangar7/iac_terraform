09dfc56f5680bf1015035d2f16b6fffffe30ffe2
Harshal Dhangar <harshaldhangar@gmail.com>
2026-04-05 08:49:20 +0000

    list type

---
+++ iac/terraform/day2/list_var.tf
@@ -0,0 +1,15 @@
+variable users {
+type = list
+}
+
+output printfirst {
+value = "First user is ${var.users[0]}"
+}
+
+output printsecond {
+value = "Second user is ${var.users[1]}"
+}
+
+output printthird {
+value = "Third user is ${var.users[2]}"
+}
